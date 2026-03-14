1b9b8958b8ca95f77553f682658e233e3b534740
Tobi <omotadetobiloba@gmail.com>
2026-03-14 15:34:07 +0100

    infra: HTTPS listener + api.userail.money

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -415,7 +415,23 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  # Redirect HTTP → HTTPS once you add a cert; for now forward directly
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:us-east-1:605894285151:certificate/4c0b3d80-ffc8-4846-a45e-0922bca0d192"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
