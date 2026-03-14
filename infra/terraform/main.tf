409e9d5685325bd7daf1a66e8aa64cbd85148c24
Tobi <omotadetobiloba@gmail.com>
2026-03-14 15:55:37 +0100

    infra: remove NAT gateway (~/mo saving)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -31,8 +31,8 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true  # cost: one NAT GW instead of two
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
 
