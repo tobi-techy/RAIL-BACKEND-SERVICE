8fe1e731b1f405a1a968c637355d99b7e82877a2
Tobi <omotadetobiloba@gmail.com>
2026-05-24 08:19:18 +0100

    fix: remove CONCURRENTLY from index migrations (incompatible with golang-migrate transactions)

--- migrations/215_recurring_expense_indexes.down.sql
+++ migrations/215_recurring_expense_indexes.down.sql
@@ -1,2 +1,2 @@
-DROP INDEX CONCURRENTLY IF EXISTS idx_card_transactions_recurring;
-DROP INDEX CONCURRENTLY IF EXISTS idx_p2p_transfers_recurring;
+DROP INDEX IF EXISTS idx_card_transactions_recurring;
+DROP INDEX IF EXISTS idx_p2p_transfers_recurring;
